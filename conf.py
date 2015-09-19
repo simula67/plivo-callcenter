be6fcf068e218d26963d24f0ad1df76995f8b5c5
Joji Antony <simula67@gmail.com>
2015-09-20 00:20:33 +0530

    prepare for heroku deploy

--- conf.py
+++ conf.py
@@ -1,11 +1,11 @@
 import os
 
-APP_URL = "http://5b58e10c.ngrok.io"
+APP_URL = "plivocallcenter.herokuapp.com"
 
 # Plivo Auth ID and Auth Token
 PLIVO_AUTH_ID = os.environ.get('PLIVO_AUTH_ID')
 PLIVO_AUTH_TOKEN = os.environ.get('PLIVO_AUTH_TOKEN')
-
-DB_HOST="localhost"
-DB_NAME="plivo"
+DB_USERNAME=os.environ.get("DB_USERNAME")
+DB_HOST=os.environ.get("DB_HOST")
+DB_NAME=os.environ.get("DB_NAME")
 DB_PASSWD=os.environ.get('DB_PASSWD')
