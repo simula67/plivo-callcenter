b58e131031b09c6baa6399f175394b937c0f1b5e
Joji Antony <simula67@gmail.com>
2015-09-20 13:33:28 +0530

    more prettification and remove SQL injection

--- app.py
+++ app.py
@@ -75,9 +75,9 @@ def answer():
         free_agent_sip_username= None
         response = generate_queue_response()
     if free_agent_sip_username is None:
-        execute_query("INSERT INTO calls (calluuid, agent_sipusername) VALUES('{}', {});".format(call_uuid, "NULL"))
+        execute_query("INSERT INTO calls (calluuid, agent_sipusername) VALUES(%s, %s);", call_uuid, "NULL")
     else:
-        execute_query("INSERT INTO calls (calluuid, agent_sipusername) VALUES('{}', '{}');".format(call_uuid, free_agent_sip_username))
+        execute_query("INSERT INTO calls (calluuid, agent_sipusername) VALUES(%s, %s);", call_uuid, free_agent_sip_username)
     return response
 
 @app.route("/hangup", methods=['POST', 'GET'])
@@ -89,12 +89,12 @@ def hangup():
     # First, lets store some stats
     execute_query("INSERT INTO call_stats (duration) VALUES (%s)", call_duration)
     # Now on with business
-    cursor = execute_query("SELECT agent_sipusername FROM calls WHERE calluuid='{}' AND agent_sipusername IS NOT NULL".format(call_uuid))
+    cursor = execute_query("SELECT agent_sipusername FROM calls WHERE calluuid=%s AND agent_sipusername IS NOT NULL", call_uuid)
     if cursor.rowcount > 0:
         # Hung up call was being handled by an agent. Mark him/her free
         agent_sipusername = cursor.fetchone()[0]
-        execute_query("UPDATE agents SET busy='false' WHERE sipusername = '{}'".format(agent_sipusername))
-        transfer_calluuid_cursor = execute_query("SELECT calluuid FROM calls WHERE calluuid not like '{}' AND agent_sipusername IS NULL ORDER BY id".format(call_uuid))
+        execute_query("UPDATE agents SET busy='false' WHERE sipusername = %s",agent_sipusername)
+        transfer_calluuid_cursor = execute_query("SELECT calluuid FROM calls WHERE calluuid not like %s AND agent_sipusername IS NULL ORDER BY id",call_uuid)
         if transfer_calluuid_cursor.rowcount > 0:
             # There is atleast one call that is in queue
             transfer_calluuid = transfer_calluuid_cursor.fetchone()[0]
@@ -104,9 +104,9 @@ def hangup():
             }
             p.transfer_call(params)
             # Transferred call will get new uuid, So delete current one
-            execute_query("DELETE FROM calls WHERE calluuid ='{}'".format(transfer_calluuid))
+            execute_query("DELETE FROM calls WHERE calluuid =%s",transfer_calluuid)
     # Delete the original call from the database, It was hung up
-    execute_query("DELETE FROM calls WHERE calluuid ='{}'".format(call_uuid))
+    execute_query("DELETE FROM calls WHERE calluuid =%s",call_uuid)
     return ""
 
 @app.route("/queuelen", methods=["GET","POST"])
@@ -126,12 +126,12 @@ def admin():
         available_sips = [i[0] for i in available_sip_rows]
         return render_template("admin_page.html", average_duration=average_duration, sips=available_sips)
     elif request.method == "POST":
-        execute_query("INSERT INTO agents (sipusername, busy) VALUES ('{}', 'false')".format(request.form['sipusername']))
+        execute_query("INSERT INTO agents (sipusername, busy) VALUES (%s, 'false')",request.form['sipusername'])
         return redirect(url_for('admin'))
 
 @app.route("/admin/deleteagent", methods=['POST', 'GET'])
 def delete_agent():
-    execute_query("DELETE FROM agents WHERE sipusername='{}'".format(request.form['agentUsername']))
+    execute_query("DELETE FROM agents WHERE sipusername=%s", request.form['agentUsername'])
     return ""
 def generate_forward_response(sip_username):
     plivo_response = plivo.XML.Response()
@@ -159,6 +159,6 @@ def get_free_agent():
     return cursor.fetchone()[0]
 
 def mark_agent_busy(free_agent_sip_username):
-    execute_query("UPDATE agents SET busy='true' WHERE sipusername='{}'".format(free_agent_sip_username))
+    execute_query("UPDATE agents SET busy='true' WHERE sipusername=%s",free_agent_sip_username)
 
 app.run(host="0.0.0.0", port=int(os.environ.get("PORT")))
